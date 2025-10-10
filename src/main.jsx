427e689dfc07be932c60508ac4110b6520730064
asifjalal1 <76102267+asifjalal1@users.noreply.github.com>
2025-10-10 22:27:06 +0500

    new pkg

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.jsx'
-import Scrarpping from './App.jsx'
+import Scrapping from './Scrapping.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Scrarpping /> 
+    <Scrapping /> 
     {/* <App /> */}
 
   </StrictMode>,
